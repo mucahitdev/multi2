37784e91c0295ab044c261180525832ae5c9135b
Mücahit Kökdemir <76880536+mucahitdev@users.noreply.github.com>
2025-08-20 21:55:46 +0300

    Initial Commit

---
+++ multi2/multi2App.swift
@@ -0,0 +1,17 @@
+//
+//  multi2App.swift
+//  multi2
+//
+//  Created by MÜCAHİT KÖKDEMİR on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct multi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
